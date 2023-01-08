2f2550248914e47a6ab439da1620687d95abebf2
Sumit Panchal <112932064+Sumitpanchal1993@users.noreply.github.com>
2023-01-08 21:32:49 +0530

    Sunday Work

---
+++ src/Common Components/Carousel/Carousel.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../Carousel/Carousel.css'
+
+const caro= document.getElementById('slider').
+
+console.log(caro)
+
+
+export default function Carousel() {
+
+
+
+  return (
+    <>
+     <div className="caro" id='slider'>
+        <button> &lt;</button>
+        <button> &gt;</button>
+     </div> 
+    </>
+  )
+}
